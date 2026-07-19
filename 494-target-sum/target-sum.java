3206d1589b9095049c386b37c931417b5f562353
Dhivya.S <sdya2005@gmail.com>
2026-07-19 11:35:49 +0530

    Time: 2 ms (99.94%) | Memory: 43.3 MB (61.96%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,20 +1,24 @@
 class Solution {
+
     public int findTargetSumWays(int[] nums, int target) {
-               return solve(nums, target, 0, 0);
-    }
 
-    public int solve(int[] nums, int target, int index, int sum) {
+        int sum = 0;
+        for (int n : nums) sum += n;
 
-        if (index == nums.length) {
-            if (sum == target)
-                return 1;
-            else
-                return 0;
+        if ((sum + target) % 2 != 0 || Math.abs(target) > sum)
+            return 0;
+
+        int s = (sum + target) / 2;
+
+        int[] dp = new int[s + 1];
+        dp[0] = 1;
+
+        for (int num : nums) {
+            for (int j = s; j >= num; j--) {
+                dp[j] += dp[j - num];
+            }
         }
 
-        int plus = solve(nums, target, index + 1, sum + nums[index]);
-        int minus = solve(nums, target, index + 1, sum - nums[index]);
-
-        return plus + minus;
+        return dp[s];
     }
 }
